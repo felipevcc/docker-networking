cd8b21f40099b5021dbba87fea7c8c86bb87e904
felipevcc <felipevillamizarc@gmail.com>
2023-11-23 12:24:27 -0500

    Database connection

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const MongoClient = require('mongodb').MongoClient;
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/docker_test';
+const { MongoClient } = require('mongodb');
+const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
-app.get('/', (_req, res) => {
-  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (error, db) => {
-    if (error) {
-      res.status(500).send('❌ [31mError[39m DB: ' + error);
-    } else {
-      res.send('✅ [32mConnected[39m DB');
-      db.close();
-    }
-  });
+app.get('/', async (_req, res) => {
+  try {
+    const client = new MongoClient(mongoUri, {serverSelectionTimeoutMS: 3000});
+    await client.connect();
+    await client.close();
+    res.send('✅ Connected DB');
+  } catch (error) {
+    res.status(500).send('❌ Error DB: ' + error);
+  }
 });
 
 app.listen(PORT, () => console.log(`✅ [32mServer[39m listening on PORT ${PORT}`));
